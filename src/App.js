034bbc240315589d76e65edb90a5d507402abbc8
Yuvraj Bhonsle <yuvrajyhb@gmail.com>
2023-08-16 11:45:44 +0530

    adding files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
-import React from 'react';
+import React from "react";
 import Navbar from "./Components/Navbar";
-import Contact from './Components/Contact';
-import About from './Components/About'
-import Homepage from './Components/Homepage';
-import Footer from './Components/Footer';
-import "./main.css"
+import About from "./Components/About";
+import Homepage from "./Components/Homepage";
+import Footer from "./Components/Footer";
+import IndustrialAutomation from "./pages/IndustrialAutomation";
+import AdvancedIOT from "./pages/AdvancedIOT";
+import IndustrialElectrical from "./pages/IndustrialElectrical";
+// import "./main.css"
+import "./App.css";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Portfolio from './pages/Portfolio';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import BuildingManagement from "./pages/BuildingManagement";
+import ProductProcess from "./pages/ProductProcess";
+import ROPlant from "./pages/ROPlant";
+import SewageTreatment from "./pages/SewageTreatment";
+import EnergyManagement from "./pages/EnergyManagement";
+import MachineHealth from "./pages/MachineHealth";
+import ConnectedLogistics from "./pages/ConnectedLogistics";
+import AssetManagement from "./pages/AssetManagement";
 function App() {
-	return (
-		<BrowserRouter>
-			<React.Fragment>
-				<Navbar />
-				<Routes>
-					<Route path='/' element={<Homepage />}></Route>
-					<Route path='/about' element={<About />}></Route>
-					<Route path='/portfolio' element={<Portfolio />}></Route>
-					<Route path='/contact' element={<Contact />}></Route>
-				</Routes>
-			</React.Fragment>
-			<Footer/>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <React.Fragment>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            {/* <Route path='/portfolio' element={<Portfolio />}></Route> */}
+            <Route
+              path="/industrialautomation"
+              element={<IndustrialAutomation />}
+            ></Route>
+            <Route path="/advancediot" element={<AdvancedIOT />}></Route>
+            <Route
+              path="/industrialelectrical"
+              element={<IndustrialElectrical />}
+            ></Route>
+            <Route
+              path="/buildingManagement"
+              element={<BuildingManagement />}
+            ></Route>
+            <Route path="/productProcess" element={<ProductProcess />}></Route>
+            <Route path="/roPlant" element={<ROPlant />}></Route>
+            <Route path="/sewage" element={<SewageTreatment />}></Route>
+            <Route
+              path="/energyManagement"
+              element={<EnergyManagement />}
+            ></Route>
+            <Route path="/machineHealth" element={<MachineHealth />}></Route>
+            <Route path="/logistics" element={<ConnectedLogistics />}></Route>
+            <Route
+              path="/assetManagement"
+              element={<AssetManagement />}
+            ></Route>
+            <Route path="/contactpage" element={<ContactPage />}></Route>
+            {/* <Route path="/contactpage" element={<AdressPage />}></Route> */}
+          </Routes>
+        </React.Fragment>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import './contact.css'
+import React from 'react';
+import { useSpring, animated } from 'react-spring';
+import { useInView } from 'react-intersection-observer';
+import './contact.css';
+
+function useFadeInAnimation(inView) {
+  return useSpring({
+    opacity: inView ? 1 : 0,
+    y: inView ? 0 : 30,
+    from: { opacity: 0, y: 30 },
+    config: { duration: 600 },
+  });
+}
+
 function Contact() {
-  const handleClick=()=>{
-    document.getElementById("sucess").innerHTML='We have got your email!!! ';
-    console.log('cliked')
-  }
+  const [contactRef, contactInView] = useInView({
+    triggerOnce: false,
+    threshold: 0,
+    trackVisibility: true,
+    delay: 400,
+  });
+
+  const handleClick = () => {
+    let inputVal = document.getElementById("input");
+    if (inputVal && inputVal.value) {
+      document.getElementById("success").innerHTML = 'We have got your email!!! ';
+      document.getElementById("error").innerHTML = '';
+    } else {
+      document.getElementById("error").innerHTML = 'Please enter your email!';
+      document.getElementById("success").innerHTML = '';
+    }
+    console.log('clicked')
+  };
+
   return (
-    <div className='contact-section' id='contact'>
-      <h1>Stay In Touch</h1>
-      <p>Enter your email address to register to our newsletter subscription!</p>
-      <input type="text" placeholder='Enter your email adress' />
-      <button onClick={handleClick} >Subscribe</button>
-      <span id='sucess'></span>
-    </div>
+    <animated.div className='contact-section' id='contact' ref={contactRef} style={useFadeInAnimation(contactInView)}>
+      <div className="contact-container">
+        <h1>Stay In Touch</h1>
+        <p>Enter your email address to register to our newsletter subscription!</p>
+        <input type="text" id='input' placeholder='Enter your email address' />
+        <button onClick={handleClick}>Subscribe</button>
+        <span id='success'></span>
+        <span id='error'></span>
+      </div>
+    </animated.div>
   )
 }
-export default Contact
+
+export default Contact;
